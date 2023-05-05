339688bc3b66c765a732117202469b88ffa06576
helga-umrikh <zetaumr@gmail.com>
2023-05-06 02:04:34 +0300

    refactor: delete and change script files

--- script.js
+++ script.js
@@ -1,137 +1,121 @@
-let images = [{
-  url: "./asserts/image_1.jpg"
-}, {
-  url: "./asserts/image_2.jpg"
-}, {
-  url: "./asserts/image_3.jpg"
-}];
+const entities = [
+  {
+    url: "./asserts/image_1.jpg",
+    menuBtn: "ROSTOV-ON-DON, ADMIRAL",
+    city: "Rostov-on-Don LCD admiral",
+    area: "81 m2",
+    time: "3.5 months"
+  },
 
+  {
+    url: "./asserts/image_2.jpg",
+    menuBtn:  "SOCHI THIEVES",
+    city: "Sochi Thieves",
+    area: "105 m2",
+    time: "4 months"
+  },
 
+  {
+    url: "./asserts/image_3.jpg",
+    menuBtn: "ROSTOV-ON-DON PATRIOTIC",
+    city: "Rostov-on-Don Patriotic",
+    area: "93 m2",
+    time: "3 months"
+  }
+];
 
-let text = ["ROSTOV-ON-DON, ADMIRAL", "SOCHI THIEVES", "ROSTOV-ON-DON PATRIOTIC"];
-let cityText = ["Rostov-on-Don LCD admiral", "Sochi Thieves", "Rostov-on-Don Patriotic"];
-let apartmentArea = ["81 m2", "105 m2", "93 m2"];
-let repairTime= ["3.5 months", "4 months", "3 months"];
+function initSlider() {
 
-function initSlider(options) {
-  if (!images || !images.length) return;
+  let currIndex = 0;
 
-  options = options || {
-    dots: true,
-    autoplay: false
+  //ноды
+  const sliderBox =  document.querySelector(".compl-projects");
+  const imageBox = document.querySelector(".projects-image");
+  const menuBox = document.querySelector(".pr-nav");
+
+  //СМЕНА КАРТИНКИ//
+  function initImages(index) {
+    imageBox.style.backgroundImage = `url(${entities[index].url})`;
   };
+  initImages(currIndex);
 
-  let sliderImages = document.querySelector(".projects-image");
-  let sliderArrows = document.querySelector(".arrow-symbol");
-  let sliderDots = document.querySelector(".dot-symbols");
-  let sliderMenu = document.querySelector(".pr-nav");
-  let projectDetails = document.querySelector(".projects_items");
-
-  
-  initImages();
-  initArrows();
-  initMenu();
-  // addProjectDetails();
-
-  if (options.dots) {
-    initDots();
-  }
-  
-  if (options.autoplay) {
-    initAutoplay();
-  }
-
-  function initImages() {
-    images.forEach((image, index) => {
-      let imageDiv =`<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index = "${index}"></div>`;
-      sliderImages.innerHTML += imageDiv;
-
-      let cityContainer = projectDetails.querySelector(".city");
-      cityContainer.innerText = `${cityText[index]}`;
-    })
-  }
-
-  function initArrows() {
-    sliderArrows.querySelectorAll(".slider-arrow").forEach(arrow => {
-      arrow.addEventListener("click", function() {
-        let curNumber = +sliderImages.querySelector(".active").dataset.index;
-        let nextNumber;
-        if (arrow.classList.contains("arrow_left")) {
-          nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
-        } else {
-          nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
-        }
-        moveSlider(nextNumber);
-      });
-    })
-  }
-
-  function initDots() {
-    images.forEach((image, index) => {
-      let dot = `<div class="dot-symbol n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
-      sliderDots.innerHTML += dot;
-    });
-    sliderDots.querySelectorAll(".dot-symbol").forEach(dot => {
-      dot.addEventListener("click", function() {
-        moveSlider(this.dataset.index);
-      })
-    })
-  }
-
+  //НАВИГАЦИЯ МЕНЮ
   function initMenu() {
-    images.forEach((image, index) => {
-      let menuText = `<li class="pr-nav-item yeseva-font">
-      <a class="slider-menu add-border n${index} ${index === 0? "active-title" : ""}"
-      data-index="${index}"" href="#">${text[index]}</a>
-      </li>`;
-      sliderMenu.innerHTML += menuText;
+    entities.forEach((option, index) => {
+      let menuText = `<li class= "pr-nav-item yeseva-font"><a class="slider-menu add-border" data-index="${index}">${entities[index].menuBtn}</a></li>`;
+      menuBox.innerHTML += menuText;
     });
-    sliderMenu.querySelectorAll(".slider-menu").forEach(option => {
+
+    //обработчик для смены слайдов
+    menuBox.querySelectorAll(".slider-menu").forEach(option => {
       option.addEventListener("click", function() {
-        moveSlider(this.dataset.index);
+        initImages(this.dataset.index);
+        initProjectDetails(this.dataset.index);
+      });
+    });
+  };
+  initMenu();
+
+  const cityItem = document.querySelector(".city");
+  const areaItem = document.querySelector(".area");
+  const timeItem = document.querySelector(".time");
+
+  //СМЕНА ДАННЫХ ПРОЕКТОВ
+  function initProjectDetails(currIndex) {
+    cityItem.textContent = entities[currIndex].city;
+    areaItem.textContent = entities[currIndex].area;
+    timeItem.textContent = entities[currIndex].time;
+  };
+  initProjectDetails(currIndex);
+
+  //СТРЕЛКИ  НОДЫ//
+  const leftArr = sliderBox.querySelector(".arrow_left");
+  const rightArr = sliderBox.querySelector(".arrow_right");
+
+  //ПЕРЕКЛЮЧЕНИЕ СТРЕЛОК
+  function initArrows() {
+    leftArr.addEventListener("click", function() { 
+      if(currIndex === 0) {
+        currIndex = 2;
+      }else if (currIndex > 0) {
+        currIndex = currIndex - 1;
+      };
+  
+      initImages(currIndex);
+      initProjectDetails(currIndex);
+    });
+
+    rightArr.addEventListener("click", function() {
+      if(currIndex === 2) {
+        currIndex = 0; 
+      }else if (currIndex < 2) {
+        currIndex = currIndex + 1;
+      };
+      initImages(currIndex);
+      initProjectDetails(currIndex);
+    });
+  };
+  initArrows();
+
+  const dotsBox = document.querySelector(".dot-symbols");
+
+  // ПЕРЕКЛЮЧЕНИЕ ТОЧКАМИ
+  function initDots() {
+    entities.forEach((option, index) => {
+      let dot = `<div class="dot-symbol" data-index="${index}"></div>`;
+      dotsBox.innerHTML += dot;
+    });
+
+    dotsBox.querySelectorAll(".dot-symbol").forEach(dot => {
+      dot.addEventListener("click", function() {
+        initImages(this.dataset.index);
+        initProjectDetails(this.dataset.index);
       })
     })
-  }
-
-  // function addProjectDetails() {
-  //   images.forEach((image, index) => {
-  //     let cityContainer = projectDetails.querySelector(".city");
-  //     cityContainer.innerText = `${cityText[index]}`;
-  //   });
-  // }
-
-  function moveSlider(num) {
-    sliderImages.querySelector(".active").classList.remove("active");
-    sliderImages.querySelector(".n" + num).classList.add("active");
-
-    if (options.dots) {
-      sliderDots.querySelector(".active").classList.remove("active");
-      sliderDots.querySelector(".n" + num).classList.add("active");
-    }
-
-    sliderMenu.querySelector(".active-title").classList.remove("active-title");
-    sliderMenu.querySelector(".n" + num).classList.add("active-title");
-
-  }
-
-  function initAutoplay() {
-    setInterval(() => {
-      let curNumber = +sliderImages.querySelector(".active").dataset.index;
-      let nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
-      moveSlider(nextNumber);
-    }, options.autoplayInterval);
   };
-
-}
-
-
-
-let sliderOptions = {
-  dots: true,
-  autoplay: true,
-  autoplayInterval: 5000
+  initDots();
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-  initSlider(sliderOptions);
-});
+  initSlider();
+})
